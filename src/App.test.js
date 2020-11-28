b49eae72d33c76c3458aba423db5f76727d9ee67
Tom Brown <Thomas.Brown-CIC-UK@ibm.com>
2020-11-28 17:52:59 +0000

    Fix test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const headingElement = screen.getByText(/Learn Github Actionb/i);
+  const headingElement = screen.getByText(/Learn Github Actions/i);
   expect(headingElement).toBeInTheDocument();
 });
