13876d25d5b71e6436953c598f428d2ac09d0cd7
Tom Brown <Thomas.Brown-CIC-UK@ibm.com>
2020-11-29 18:34:58 +0000

    ci(actions): fix async

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -3524,24 +3524,26 @@ exports.getState = getState;
 const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
-try {
-  const token = core.getInput('token');
-  const title = core.getInput('title');
-  const body = core.getInput('body');
-  const assignees = core.getInput('assignees');
+(async () => {
+  try {
+    const token = core.getInput('token');
+    const title = core.getInput('title');
+    const body = core.getInput('body');
+    const assignees = core.getInput('assignees');
 
-  const octokit = github.getOctokit(token);
-  const response = octokit.issues.create({
-    ...github.context.repo,
-    title,
-    body,
-    assignees: assignees ? assignees.split('\n') : undefined,
-  });
+    const octokit = await github.getOctokit(token);
+    const response = octokit.issues.create({
+      ...github.context.repo,
+      title,
+      body,
+      assignees: assignees ? assignees.split('\n') : undefined,
+    });
 
-  core.setOutput('issue', JSON.stringify(response.data));
-} catch (error) {
-  core.setFailed(error.message);
-}
+    core.setOutput('issue', JSON.stringify(response.data));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
 
 
 /***/ }),
